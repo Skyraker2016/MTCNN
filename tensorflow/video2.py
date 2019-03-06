5cab1358c1a8b167f57e75088605aafe4755eb93
Skyraker <13925111409@163.com>
2019-03-06 10:49:46 +0800

    lable when watch

--- tensorflow/video2.py
+++ tensorflow/video2.py
@@ -18,6 +18,8 @@ def catch_video(video_path, output_path, video_id, bbox_path='./meta.txt', min_s
     cap=cv2.VideoCapture(video_path)
     video_name = str(video_id)
     index = -1
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
     while True:
         # 取帧
@@ -46,18 +48,31 @@ def catch_video(video_path, output_path, video_id, bbox_path='./meta.txt', min_s
         # 保存
         save_name = video_name + '_' + str(index)
         print(index, ' total box:', len(bbox))
-        cv2.imwrite( output_path + save_name+'.jpg', img)
+        tmp_img = img.copy()
         show_img = None
         for face_i, box in enumerate(bbox):
             box = box.astype('int32')
-            f.write("%s\t%d\t%d\t%d\t%d\n" % (output_path + save_name, box[1], box[0], box[3], box[2]))
+            # f.write("%s\t%d\t%d\t%d\t%d\n" % (output_path + save_name, box[1], box[0], box[3], box[2]))
             show_img = cv2.rectangle(img, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 3)
             show_img = cv2.putText(img, str(face_i), (box[1], box[0]), cv2.FONT_HERSHEY_PLAIN, 2.0, (0, 0, 255), 2)
-        if show:
-            cv2.imshow('img', show_img)
-            cv2.waitKey(1)
+        # if show:
+        
+        flag = False
+        cv2.imshow('img', show_img)
+        cv2.waitKey(1)
+        for face_i, box in enumerate(bbox):
+            emotion = input("face %d: " % (face_i, ))
+            try:
+                emotion_id = int(emotion)
+                f.write("%s\t%d\t%d\t%d\t%d\t%d\n" % (output_path + save_name, box[1], box[0], box[3], box[2], emotion_id))
+                flag = True # 如果没有一个表情成功，则不保留该图像
+            except:
+                print("Not emotion")
+                continue
         f.close()
-        cv2.imwrite(output_path + 'bbox_' + save_name+'.jpg', show_img)
+        if flag:
+            cv2.imwrite( output_path + save_name+'.jpg', tmp_img)
+        # cv2.imwrite(output_path + 'bbox_' + save_name+'.jpg', show_img)
 
 if __name__ == '__main__':
     catch_video('ep40.mp4', './100040/', 100040)
